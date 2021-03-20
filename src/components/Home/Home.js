e4a300c2e2be37aeb609cb18563b104b03e4b47a
Ola Assem <ola.assem@viget.com>
2021-03-20 17:07:29 +0200

    imports useState

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,15 @@
+import { useState }  from 'react';
+
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log('Button clicked!', e)
-    }
+    const handleClick = () => {
 
-    const handleClickAgain = (name, e) => {
-        console.log(`hello ${name}!`, e.target)
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={ handleClick }>Click Me</button>
-            <button onClick={ (e) => handleClickAgain('Mario', e) }> Click me again</button>
         </div>
      );
 }
