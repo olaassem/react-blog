79c584e9c2d724781d35b27f90f487a0e38cb48f
Ola Assem <ola.assem@viget.com>
2021-03-20 21:06:24 +0200

    cleans up home file for fetching data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,28 +3,7 @@ import { useState, useEffect }  from 'react';
 import BlogList from '../BlogList/BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            title: 'Outwitting Squirrels',
-            body: 'Lorem ipsum...',
-            author: 'Jack Heise',
-            id: 1
-        },
-        {
-            title: 'Talking with Trees',
-            body: 'Lorem ipsum...',
-            author: 'P. L. McSweeny',
-            id: 2
-        },
-        {
-            title: 'A Passion for Donkeys',
-            body: 'Lorem ipsum...',
-            author: 'Dr. Elisabeth Svendson',
-            id: 3
-        }
-    ]);
-
-    const [name, setName] = useState('Mario');
+    const [blogs, setBlogs] = useState( null );
 
     const handleDelete = ( id ) => {
         const newBlogs = blogs.filter( blog => blog.id !== id );
@@ -34,11 +13,9 @@ const Home = () => {
     //runs after every render
     //dont change state inside bc of cont' loop effect
     //an empty array : if only want to run the function once on the 1st render
-    //
     useEffect( () => {
         console.log( 'useEffect RAN!' )
-        console.log( name )
-    }, [ name ])
+    }, [ ])
     
 
     return ( 
@@ -48,8 +25,6 @@ const Home = () => {
                 title="All Blogs!"
                 handleDelete={ handleDelete } 
             />
-            <button onClick={() => setName('Luigi')}>Change Name</button>
-            <p>{ name }</p>
         </div>
      );
 }
