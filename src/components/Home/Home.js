d190afd1fa5a4a16f402ad57b25fcd366f343057
Ola Assem <ola.assem@viget.com>
2021-03-20 17:02:32 +0200

    reacting to e in comps

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,18 @@
 const Home = () => {
 
-    const handleClick = () => {
-        console.log('Button clicked!')
+    const handleClick = (e) => {
+        console.log('Button clicked!', e)
     }
 
-    const handleClickAgain = (name) => {
-        console.log(`hello ${name}!`)
+    const handleClickAgain = (name, e) => {
+        console.log(`hello ${name}!`, e.target)
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={ handleClick }>Click Me</button>
-            <button onClick={ () => handleClickAgain('Mario') }> Click me again</button>
+            <button onClick={ (e) => handleClickAgain('Mario', e) }> Click me again</button>
         </div>
      );
 }
