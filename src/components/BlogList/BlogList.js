3c22a2791c2b3df97b1e63a9ca390e316894c68f
Ola Assem <ola.assem@viget.com>
2021-03-20 19:58:23 +0200

    destructuring props atts

--- src/components/BlogList/BlogList.js
+++ src/components/BlogList/BlogList.js
@@ -1,10 +1,12 @@
-const BlogList = ( props ) => {
-    const title = props.title;
+const BlogList = ( {blogs, title} ) => {
+
+    // const blogs = props.blog;
+    // const title = props.title;
 
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
-            {props.blogs.map(blog => (
+            {blogs.map(blog => (
                 <div className="blog-preview" key={ blog.id }>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
