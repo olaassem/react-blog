a0a9a016e8d22c03566abed859fd79dcdab49cbe
Ola Assem <ola.assem@viget.com>
2021-03-20 16:54:39 +0200

    function reference

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Button clicked!')
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
         </div>
      );
 }
