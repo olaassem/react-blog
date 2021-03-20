b289b0bc433ae7a8c087a6a890287fd6980fa255
Ola Assem <ola.assem@viget.com>
2021-03-20 17:34:48 +0200

    more useState hooks for reactive values

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,15 +3,17 @@ import { useState }  from 'react';
 const Home = () => {
 
     const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(25);
 
     const handleClick = () => {
         setName('Luigi');
+        setAge(30);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            { name }
+            <p>{ name } is { age } years old.</p>
             <button onClick={ handleClick }>Click Me</button>
         </div>
      );
