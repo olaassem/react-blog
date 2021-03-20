67e37e45a474bc4aa5ef2a1d1daa0bd6e451cedd
Ola Assem <ola.assem@viget.com>
2021-03-20 19:56:03 +0200

    passes title

--- src/components/BlogList/BlogList.js
+++ src/components/BlogList/BlogList.js
@@ -1,14 +1,14 @@
 const BlogList = ( props ) => {
-    // const blogList = props.blogs;
-    // // console.log(blogList)
+    const title = props.title;
 
     return ( 
         <div className="blog-list">
+            <h2>{ title }</h2>
             {props.blogs.map(blog => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    <p>{blog.body}</p>
+                <div className="blog-preview" key={ blog.id }>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    <p>{ blog.body }</p>
                 </div>
             ))}
         </div>
