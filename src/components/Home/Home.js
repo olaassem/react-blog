335e30c2f8ed7e099c391a0c23ea33a52d794cf7
Ola Assem <ola.assem@viget.com>
2021-03-20 16:59:10 +0200

    anon function

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,10 +4,15 @@ const Home = () => {
         console.log('Button clicked!')
     }
 
+    const handleClickAgain = (name) => {
+        console.log(`hello ${name}!`)
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick={handleClick}>Click Me</button>
+            <button onClick={ handleClick }>Click Me</button>
+            <button onClick={ () => handleClickAgain('Mario') }> Click me again</button>
         </div>
      );
 }
