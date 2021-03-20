672c762225861c0e691c32061982e009bf558fc9
Ola Assem <ola.assem@viget.com>
2021-03-20 17:56:10 +0200

    outputting blog list

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -25,6 +25,14 @@ const Home = () => {
     return ( 
         <div className="home">
 
+           {blogs.map(blog => (
+               <div key={blog.id}>
+                   <h2>{blog.title}</h2>
+                   <p>Written by {blog.author}</p>
+                   <p>{blog.body}</p>
+               </div>
+           ))} 
+
         </div>
      );
 }
