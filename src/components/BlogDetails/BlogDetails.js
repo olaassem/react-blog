f445f331af53f46204a04db953af86cecee9084a
Ola Assem <ola.assem@viget.com>
2021-03-21 20:09:31 +0200

    useParams to specify id

--- src/components/BlogDetails/BlogDetails.js
+++ src/components/BlogDetails/BlogDetails.js
@@ -1,7 +1,12 @@
+import { useParams } from "react-router";
+
 const BlogDetails = () => {
+    //grab route parameters from the route.
+    //we defined :id in the App.js blogdetails route:
+    const { id } = useParams();
     return ( 
         <div className="blog-details">
-            <h2>Blog Details</h2>
+            <h2>Blog Details - { id }</h2>
         </div>
      );
 }
