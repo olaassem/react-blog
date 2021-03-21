300d8af2596248e82c4d9dd98c0e2e9f9c33fee0
Ola Assem <ola.assem@viget.com>
2021-03-21 22:17:07 +0200

    controlled form inputs

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [ title, setTitle ] = useState('');
+    const [ author, setAuthor ] = useState('');
+    const [ body, setBody ] = useState('');
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
@@ -7,17 +13,27 @@ const Create = () => {
                 <input 
                     type="text" 
                     required
+                    value={ title }
+                    onChange={ (e) => setTitle( e.target.value ) }
                 />
                 <label>Blog body:</label>
                 <textarea 
                     required
+                    value={ body }
+                    onChange={ (e) => setBody( e.target.value ) }
                 >
                 </textarea>
                 <label>Author:</label>
-                <select>
+                <select
+                    onChange={ (e) => setAuthor( e.target.value ) }
+                >
                     <option value="mario">Mario</option>
                     <option value="luigi">Luigi</option>
                 </select>
+                <button>Add Blog</button>
+                <p>{author}</p>
+                <p>{body}</p>
+                <p>{title}</p>
             </form>
         </div> 
     );
