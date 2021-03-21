444cad9535452410474436874ce644ebfafa2cf2
Ola Assem <ola.assem@viget.com>
2021-03-21 16:35:57 +0200

    exact match & create comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from'./components/Navbar/Navbar';
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
+import Create from './components/Create/Create';
+
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
         <div className="content">
           {/* only 1 route shows at any one time */}
           <Switch>
-            <Route path="/"><Home /></Route>
+            <Route path="/" exact><Home /></Route>
+            <Route path="/create"><Create /></Route>
           </Switch>
         </div>
       </div>
