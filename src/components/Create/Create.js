8feee93331452fb9a989366f85d702e363e216d9
Ola Assem <ola.assem@viget.com>
2021-03-21 22:41:50 +0200

    Adds default author value

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 
 const Create = () => {
     const [ title, setTitle ] = useState('');
-    const [ author, setAuthor ] = useState('');
+    const [ author, setAuthor ] = useState('Mario');
     const [ body, setBody ] = useState('');
     const [ isPending, setIsPending ] = useState( false );
     const history = useHistory();
