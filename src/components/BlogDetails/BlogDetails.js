d44f2f14bfcae69829294a951c2659e5e7d222de
Ola Assem <ola.assem@viget.com>
2021-03-21 20:17:48 +0200

    useFetch on blogdetails

--- src/components/BlogDetails/BlogDetails.js
+++ src/components/BlogDetails/BlogDetails.js
@@ -1,9 +1,12 @@
 import { useParams } from "react-router";
+import useFetch from '../../customHooks/useFetch';
 
 const BlogDetails = () => {
     //grab route parameters from the route.
     //we defined :id in the App.js blogdetails route:
     const { id } = useParams();
+
+    const { data, error, isLoading } = useFetch(`http://localhost:3000/blogs/${id}`);
     return ( 
         <div className="blog-details">
             <h2>Blog Details - { id }</h2>
